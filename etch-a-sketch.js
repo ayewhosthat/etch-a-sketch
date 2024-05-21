4824fa4a818151b636641fbc5e6a2ab96fa945ad
ayewhosthat <alan.notiverson@gmail.com>
2024-05-20 23:30:46 -0400

    Display prompt to user upon clicking the resize button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,20 +1,52 @@
-let pageBody = document.querySelector('body');
+const availableWidth = 960;
+
+const pageBody = document.querySelector('body');
+// add button that lets user resize grid
+const pageContent = document.createElement('div');
+pageContent.classList.add('content-container');
+
+// now create buttons & the button container
+const clearBtn = document.createElement('button');
+const resizeBtn = document.createElement('button');
+
+clearBtn.textContent = 'Clear';
+resizeBtn.textContent = 'Resize';
+
+const buttons = document.createElement('div')
+buttons.classList.add('button-container');
+resizeBtn.addEventListener('click', () => {
+    const newSize = prompt('Enter new grid size, must be less than 100: ');
+    const squares = document.querySelectorAll('.square');
+    for (let i = 0; i < squares.length; i++) {
+        const square = squares[i];
+        containerDiv.removeChild(square);
+    }
+    });
+buttons.appendChild(resizeBtn);
+buttons.appendChild(clearBtn);
+pageContent.appendChild(buttons);
+
 // fetch container div
-let containerDiv = document.querySelector('.container-div')
-const gridSize = 16;
+const containerDiv = document.querySelector('.container-div')
+createSquareGrid(16);
 
 function createSquareGrid(dimension) {
     for (let i = 1; i <= dimension; i++) {
-        let row = document.createElement('div');
+        const row = document.createElement('div');
         row.classList.add('row');
         for (let j = 1; j <= dimension; j++) {
             const square = document.createElement('div');
             square.classList.add('square');
+            square.addEventListener('mouseenter', () => {
+                square.style.backgroundColor = 'lightblue';
+            });
+            const size = availableWidth/dimension;
+            square.style.width = size + 'px';
+            square.style.height = size + 'px';
             row.appendChild(square);
         }
         containerDiv.appendChild(row);
     }
-    pageBody.appendChild(containerDiv);
+    pageContent.appendChild(containerDiv);
+    pageBody.appendChild(pageContent);
 }
-
-createSquareGrid(gridSize);
